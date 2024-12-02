ac43ad0e2d486d413506e5acb295ee5e5470eacc
junbeomwooo <junbeom2.woo@gmail.com>
2024-12-02 03:29:26 +0100

    Type definition completed

--- src/app/member/[email]/page.tsx
+++ src/app/member/[email]/page.tsx
@@ -1,14 +1,8 @@
 import MemberPage from "@/components/MemberPage";
 
-export default function page({
+export default function page({ params }: { params: { email: string } }) {
+  const { email } = params;
 
-  params,
-}: {
-  params: { email: string};
-}) {
-
-  const { email  } = params;
-  
   return (
     <>
       <MemberPage email={email} />
